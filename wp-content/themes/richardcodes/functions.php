0c166aa44e443ab6c95b76b01cbe634f6b1e3423
“Richard Middleton” <Richard@richardmiddleton.me>
2018-05-23 13:12:09 +0200

    jquery for plugins and image optimisation

--- wp-content/themes/richardcodes/functions.php
+++ wp-content/themes/richardcodes/functions.php
@@ -119,6 +119,16 @@ add_action( 'widgets_init', 'richardcodes_widgets_init' );
 /**
  * Enqueue scripts and styles.
  */
+
+ // include custom jQuery
+function shapeSpace_include_custom_jquery() {
+
+	wp_deregister_script('jquery');
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
+
+}
+add_action('wp_enqueue_scripts', 'shapeSpace_include_custom_jquery');
+
 function richardcodes_scripts() {
 	wp_enqueue_style( 'richardcodes-style', get_stylesheet_uri() );
 
